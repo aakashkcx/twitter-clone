a7ca2b9f042f376808fc5b6ebac1704a0277cd2f
Aakash Kc <aakashkcx@gmail.com>
2019-09-07 14:56:33 +0100

    import jQuery and boostrap js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.slim.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
