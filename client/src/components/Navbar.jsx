155307a80d35a744b8f37ff5ccc7f9a8a07502dc
Aakash Kc <aakashkcx@gmail.com>
2019-09-07 14:57:04 +0100

    fix navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,27 @@ class Navbar extends Component {
     state = {};
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark mb-4">
-                <div className="container justify-content-center">
-                    <span className="navbar-brand mx-0 px-0">
-                        Twitter Clone
-                    </span>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+                <div className="container">
+                    <span className="navbar-brand">Twitter Clone</span>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <div class="navbar-nav ml-auto">
+                            <span class="nav-item nav-link">Link #1</span>
+                            <span class="nav-item nav-link">Link #2</span>
+                            <span class="nav-item nav-link">Link #3</span>
+                        </div>
+                    </div>
                 </div>
             </nav>
         );
