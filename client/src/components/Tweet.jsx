aab2bdc1b768ff8ee63f68cab13bc9b9b41eefaa
Aakash Kc <aakashkcx@gmail.com>
2019-09-13 23:52:15 +0100

    smaller tweet id

--- client/src/components/Tweet.jsx
+++ client/src/components/Tweet.jsx
@@ -8,7 +8,12 @@ class Tweet extends Component {
                 <h6>@{tweet.username}</h6>
                 <p>{tweet.tweet}</p>
                 <div className="d-flex justify-content-between">
-                    <small className="text-muted">{tweet._id}</small>
+                    <small
+                        className="text-muted"
+                        style={{ fontSize: '0.7rem' }}
+                    >
+                        {tweet._id}
+                    </small>
                     <small>
                         {new Date(tweet.date).toLocaleString(undefined, {
                             dateStyle: 'medium',
