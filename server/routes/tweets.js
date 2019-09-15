ec24c664f1b5c728e597ac197432f4ab80f2dd87
Aakash Kc <aakashkcx@gmail.com>
2019-09-15 16:04:57 +0100

    add /api/tweets/user/id

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -15,6 +15,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    if (!req.params.id) return res.status(400).json({ msg: 'No tweet id.' });
+
     db.tweets.findOne({ _id: req.params.id }, (err, tweet) => {
         if (err) return res.status(500).json({ err });
         if (!tweet) return res.status(404).json({ msg: 'Tweet not Found.' });
@@ -43,6 +45,23 @@ router.post('/', (req, res) => {
             if (err) return res.status(500).json({ err });
             res.status(201).json({ createdTweet });
         });
+    });
+});
+
+router.get('/user/:id', (req, res) => {
+    if (!req.params.id) return res.status(400).json({ msg: 'No user id.' });
+
+    db.users.findOne({ _id: req.params.id }, (err, user) => {
+        if (err) return res.status(500).json({ err });
+        if (!user) return res.status(404).json({ msg: 'User not found.' });
+
+        db.tweets
+            .find({ user_id: req.params.id })
+            .sort({ date: -1 })
+            .exec((err, tweets) => {
+                if (err) return res.status(500).json({ err });
+                res.status(200).json({ tweets });
+            });
     });
 });
 
