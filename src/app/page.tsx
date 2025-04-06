2c3db7c8934d8fb9127cdad34e061d7dc7094ae3
Aakash Kc <aakashkcx@gmail.com>
2025-04-06 05:12:38 +0100

    chore: add lorem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
 export default function Home() {
-  return <h1>Twitter</h1>;
+  return (
+    <main>
+      <h1>Twitter</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        enim repellat fugiat magnam laborum facilis possimus ad blanditiis
+        beatae quos, assumenda inventore totam, unde placeat non consequuntur,
+        amet architecto. Sapiente, neque saepe harum officia id soluta autem!
+        Quas mollitia nulla totam ratione excepturi. Nobis temporibus vero,
+        voluptas dicta animi laudantium aliquid debitis natus mollitia error
+        optio vel suscipit quam soluta ipsum. Mollitia ducimus iure explicabo ex
+        reiciendis odit quia accusantium nam pariatur illum distinctio, quos
+        autem velit eum, illo voluptatum! In, inventore hic iste repellat,
+        excepturi voluptatum provident dolores quisquam quia animi quaerat ipsam
+        ex suscipit pariatur nihil, nobis ratione!
+      </p>
+    </main>
+  );
 }
