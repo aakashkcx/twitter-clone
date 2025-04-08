52eccca1071a7783accc2dd0472e8fcbea42f650
Aakash Kc <aakashkcx@gmail.com>
2025-04-08 23:41:27 +0100

    feat: move tweets page to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-export default function Home() {
+import { TweetCard } from "@/components/tweet-card";
+import { QUERIES } from "@/server/db/queries";
+
+export default async function Home() {
+  const tweets = await QUERIES.getAllTweets();
+
   return (
     <main>
-      <h1>Twitter</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
-        enim repellat fugiat magnam laborum facilis possimus ad blanditiis
-        beatae quos, assumenda inventore totam, unde placeat non consequuntur,
-        amet architecto. Sapiente, neque saepe harum officia id soluta autem!
-        Quas mollitia nulla totam ratione excepturi. Nobis temporibus vero,
-        voluptas dicta animi laudantium aliquid debitis natus mollitia error
-        optio vel suscipit quam soluta ipsum. Mollitia ducimus iure explicabo ex
-        reiciendis odit quia accusantium nam pariatur illum distinctio, quos
-        autem velit eum, illo voluptatum! In, inventore hic iste repellat,
-        excepturi voluptatum provident dolores quisquam quia animi quaerat ipsam
-        ex suscipit pariatur nihil, nobis ratione!
-      </p>
+      <div className="flex flex-col gap-3">
+        {tweets.map((tweet) => (
+          <TweetCard
+            key={tweet.tweetId}
+            user={{ username: tweet.user.username }}
+            text={tweet.text}
+            createdAt={new Date(tweet.createdAt)}
+          />
+        ))}
+      </div>
     </main>
   );
 }
