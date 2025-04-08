f3d46bddcf56c62a5da3ca891826ef850e4ff8c5
Aakash Kc <aakashkcx@gmail.com>
2025-04-09 00:19:44 +0100

    chore: don't let build block deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
