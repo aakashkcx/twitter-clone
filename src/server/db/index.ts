8aca6e31bcdb641f0f06675f9b56c72452e4a938
Aakash Kc <aakashkcx@gmail.com>
2025-04-08 20:07:50 +0100

    feat: add neon, drizzle, t3-oss/env, init drizzle schema

---
+++ src/server/db/index.ts
@@ -0,0 +1,9 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import { env } from "@/env";
+import * as schema from "@/server/db/schema";
+
+const sql = neon(env.DATABASE_URL);
+
+export const db = drizzle({ client: sql, schema });
