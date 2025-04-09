fe918990b1358d096e83b8ed608cf5d58aab5059
Aakash Kc <aakashkcx@gmail.com>
2025-04-09 05:10:41 +0100

    feat: update user page, use UserCard, get user tweets

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+import { TweetCard } from "@/components/tweet-card";
+import { UserCard } from "@/components/user-card";
+import { QUERIES } from "@/server/db/queries";
+
 export default async function UserPage({
   params,
 }: {
@@ -5,9 +12,24 @@ export default async function UserPage({
 }) {
   const { username } = await params;
 
+  const user = await QUERIES.getUserByUsername(username);
+  if (!user) return notFound();
+
+  const tweets = await QUERIES.getAllTweetsByUser(user.userId);
+
   return (
-    <div>
-      <h1>Username: {username}</h1>
-    </div>
+    <>
+      <UserCard user={user} />
+      <div className="flex flex-col gap-3">
+        {tweets.map((tweet) => (
+          <Link
+            key={tweet.tweetId}
+            href={`/@${tweet.user.username}/tweet/${tweet.tweetId}`}
+          >
+            <TweetCard tweet={tweet} />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
