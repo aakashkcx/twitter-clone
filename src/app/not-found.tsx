34728307318c9b224f370e8059988ced4b2d9b34
Aakash Kc <aakashkcx@gmail.com>
2025-04-09 03:40:16 +0100

    fix: eslint react/no-unescaped-entities

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,9 @@ export default function NotFoundPage() {
     <section className="flex grow flex-col justify-center gap-3 text-center">
       <h1 className="text-6xl font-bold tracking-tight">404</h1>
       <h3 className="text-3xl font-semibold tracking-tight">Not Found</h3>
-      <p className="text-lg">The page you're looking for doesn't exist.</p>
+      <p className="text-lg">
+        The page you&apos;re looking for doesn&apos;t exist.
+      </p>
     </section>
   );
 }
