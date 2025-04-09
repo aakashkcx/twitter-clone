7c8bad01ce4e60adb5bead587cdb3a9ed1e8b15b
Aakash Kc <aakashkcx@gmail.com>
2025-04-09 05:08:19 +0100

    feat: add user and tweet queries

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,6 +1,24 @@
 import { db } from "@/server/db";
 
 export const QUERIES = {
+  getUserByUsername(username: string) {
+    return db.query.usersTable.findFirst({
+      where: (usersTable, { eq }) => eq(usersTable.username, username),
+    });
+  },
+  getTweetById(tweetId: number) {
+    return db.query.tweetsTable.findFirst({
+      where: (tweetsTable, { eq }) => eq(tweetsTable.tweetId, tweetId),
+      with: { user: true },
+    });
+  },
+  getAllTweetsByUser(userId: number) {
+    return db.query.tweetsTable.findMany({
+      where: (tweetsTable, { eq }) => eq(tweetsTable.userId, userId),
+      with: { user: true },
+      orderBy: (tweetsTable, { desc }) => desc(tweetsTable.createdAt),
+    });
+  },
   getAllTweets() {
     return db.query.tweetsTable.findMany({
       with: { user: true },
