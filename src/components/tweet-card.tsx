b22510e6a3b4b35f0ff05c0f73382c95855922a9
Aakash Kc <aakashkcx@gmail.com>
2025-04-12 04:41:14 +0100

    feat: add display name to tweet and user cards

--- src/components/tweet-card.tsx
+++ src/components/tweet-card.tsx
@@ -4,15 +4,26 @@ export function TweetCard({
   tweet,
 }: {
   tweet: {
-    user: { username: string };
+    user: { username: string; displayName: string | null };
     text: string;
     createdAt: Date;
   };
 }) {
   return (
     <Card className="flex flex-col gap-1">
-      <CardContent className="leading-none font-semibold">
-        @{tweet.user.username}
+      <CardContent className="flex flex-row gap-3">
+        {tweet.user.displayName ? (
+          <>
+            <span className="font-semibold">{tweet.user.displayName}</span>
+            <span className="text-muted-foreground">
+              @{tweet.user.username}
+            </span>
+          </>
+        ) : (
+          <>
+            <span className="font-semibold">@{tweet.user.username}</span>
+          </>
+        )}
       </CardContent>
       <CardContent>{tweet.text}</CardContent>
       <CardContent className="text-muted-foreground text-sm">
--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -12,8 +12,17 @@ export function UserCard({
 }) {
   return (
     <Card className="flex flex-col gap-1">
-      <CardContent className="leading-none font-semibold">
-        {user.displayName} @{user.username}
+      <CardContent className="flex flex-col">
+        {user.displayName ? (
+          <>
+            <span className="font-semibold">{user.displayName}</span>
+            <span className="text-muted-foreground">@{user.username}</span>
+          </>
+        ) : (
+          <>
+            <span className="font-semibold">@{user.username}</span>
+          </>
+        )}
       </CardContent>
       <CardContent className="text-muted-foreground text-sm">
         {user.createdAt.toLocaleString()}
