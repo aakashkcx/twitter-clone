b469c52732345ae3705f47fb8b3480a3deb41f2a
Aakash Kc <aakashkcx@gmail.com>
2025-05-17 03:52:08 +0100

    feat: add jwt token apis

---
+++ src/server/auth/token.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { type JWTPayload, jwtVerify, SignJWT } from "jose";
+
+import { env } from "@/env";
+
+const TOKEN_ALGORITHM = "HS256";
+const TOKEN_SECRET = new TextEncoder().encode(env.SESSION_TOKEN_SECRET);
+const TOKEN_EXPIRY = env.SESSION_TOKEN_EXPIRY;
+
+export async function encodeToken(payload: JWTPayload) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: TOKEN_ALGORITHM })
+    .setIssuedAt()
+    .setExpirationTime(TOKEN_EXPIRY)
+    .sign(TOKEN_SECRET);
+}
+
+export async function decodeToken(token: string) {
+  try {
+    const { payload } = await jwtVerify(token, TOKEN_SECRET, {
+      algorithms: [TOKEN_ALGORITHM],
+    });
+    return payload;
+  } catch (error) {
+    return undefined;
+  }
+}
