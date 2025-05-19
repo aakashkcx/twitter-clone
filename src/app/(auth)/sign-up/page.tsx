e8cb80ba06eb726597056f082258df049548d697
Aakash Kc <aakashkcx@gmail.com>
2025-05-19 19:12:34 +0100

    refactor: clean up card ui component

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,15 +1,11 @@
 import { SignUpForm } from "@/components/auth/sign-up-form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardTitle } from "@/components/ui/card";
 
 export default function SignUpPage() {
   return (
     <Card className="w-full max-w-md self-center">
-      <CardHeader>
-        <CardTitle className="text-3xl">Sign Up</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <SignUpForm />
-      </CardContent>
+      <CardTitle className="text-3xl">Sign Up</CardTitle>
+      <SignUpForm />
     </Card>
   );
 }
