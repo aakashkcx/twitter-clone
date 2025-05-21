fc644f06121c0e1dd4cfed9621672da83f7e24a7
Aakash Kc <aakashkcx@gmail.com>
2025-05-21 21:40:11 +0100

    feat: use suspense boundary for nav links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,12 @@
 import { MessagesSquare } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
 
 import { UserAvatar } from "@/components/user-avatar";
 import { signOutAction } from "@/server/auth/actions";
 import { getCurrentUser } from "@/server/auth/user";
 
-export async function Header() {
-  const user = await getCurrentUser();
-
+export function Header() {
   return (
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 border-b py-2 backdrop-blur">
       <div className="container flex flex-row items-center justify-between">
@@ -19,45 +18,56 @@ export async function Header() {
           <span className="sr-only sm:not-sr-only sm:p-2">Twitter</span>
         </Link>
         <nav className="flex flex-row items-center gap-3 font-medium">
-          {user ? (
-            <>
-              <button
-                onClick={signOutAction}
-                className="text-muted-foreground hover:text-primary p-2 transition-colors"
-              >
-                Sign Out
-              </button>
-              <Link
-                href={`/@${user.username}`}
-                className="group flex flex-row items-center gap-1"
-              >
-                <span className="text-muted-foreground group-hover:text-primary p-2 transition-colors">
-                  @{user.username}
-                </span>
-                <UserAvatar
-                  user={user}
-                  className="size-8 saturate-25 transition-colors group-hover:saturate-100"
-                />
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link
-                href="/sign-in"
-                className="text-muted-foreground hover:text-primary p-2 transition-colors"
-              >
-                Sign In
-              </Link>
-              <Link
-                href="/sign-up"
-                className="text-muted-foreground hover:text-primary p-2 transition-colors"
-              >
-                Sign Up
-              </Link>
-            </>
-          )}
+          <Suspense>
+            <NavLinks />
+          </Suspense>
         </nav>
       </div>
     </header>
+  );
+}
+
+async function NavLinks() {
+  const user = await getCurrentUser();
+  if (user) {
+    return (
+      <>
+        <button
+          onClick={signOutAction}
+          className="text-muted-foreground hover:text-primary p-2 transition-colors"
+        >
+          Sign Out
+        </button>
+        <Link
+          href={`/@${user.username}`}
+          className="group flex flex-row items-center gap-1"
+        >
+          <span className="text-muted-foreground group-hover:text-primary p-2 transition-colors">
+            @{user.username}
+          </span>
+          <UserAvatar
+            user={user}
+            className="size-8 saturate-25 transition-colors group-hover:saturate-100"
+          />
+        </Link>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Link
+        href="/sign-in"
+        className="text-muted-foreground hover:text-primary p-2 transition-colors"
+      >
+        Sign In
+      </Link>
+      <Link
+        href="/sign-up"
+        className="text-muted-foreground hover:text-primary p-2 transition-colors"
+      >
+        Sign Up
+      </Link>
+    </>
   );
 }
