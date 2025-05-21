18cffa38a9071577122f7059b16fa9de58c2ac1c
Aakash Kc <aakashkcx@gmail.com>
2025-05-22 00:39:42 +0100

    feat: add tweet form to index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 import Link from "next/link";
 
 import { TweetCard } from "@/components/tweet-card";
+import { TweetForm } from "@/components/tweet-form";
+import { Card } from "@/components/ui/card";
 import { QUERIES } from "@/server/db/queries";
 
 export default async function HomePage() {
   const tweets = await QUERIES.getVerifiedTweetsWithUser();
 
   return (
-    <div className="flex flex-col gap-3">
-      {tweets.map(({ tweet, user }) => (
-        <Link
-          key={tweet.tweetId}
-          href={`/@${user.username}/tweet/${tweet.tweetId}`}
-        >
-          <TweetCard tweet={tweet} user={user} />
-        </Link>
-      ))}
-    </div>
+    <>
+      <Card>
+        <TweetForm placeholder="What is happening?" />
+      </Card>
+
+      <div className="flex flex-col gap-3">
+        {tweets.map(({ tweet, user }) => (
+          <Link
+            key={tweet.tweetId}
+            href={`/@${user.username}/tweet/${tweet.tweetId}`}
+          >
+            <TweetCard tweet={tweet} user={user} />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
