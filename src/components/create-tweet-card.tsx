4023f7c81fbbfcccb8b94840b4b84c7431f75b9f
Aakash Kc <aakashkcx@gmail.com>
2025-05-22 01:53:02 +0100

    feat: add user avatar to createTweetCard

--- src/components/create-tweet-card.tsx
+++ src/components/create-tweet-card.tsx
@@ -1,15 +1,22 @@
 import { CreateTweetForm } from "@/components/forms/create-tweet-form";
 import { Card } from "@/components/ui/card";
+import { UserAvatar } from "./user-avatar";
 
 export function CreateTweetCard({
+  user,
   parentId,
   placeholder,
 }: {
+  user: {
+    displayName: string;
+    email: string;
+  };
   parentId?: string;
   placeholder?: string;
 }) {
   return (
-    <Card>
+    <Card className="flex flex-row gap-4">
+      <UserAvatar user={user} className="size-20 text-3xl" />
       <CreateTweetForm parentId={parentId} placeholder={placeholder} />
     </Card>
   );
