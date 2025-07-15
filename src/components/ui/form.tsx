113175d555457990e28fd62386c07f49a375d308
Aakash Kc <aakashkcx@gmail.com>
2025-07-15 02:30:18 +0100

    style: organise imports

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -104,7 +104,9 @@ function FormLabel({
   );
 }
 
-function FormControl({ ...props }: React.ComponentProps<typeof SlotPrimitive.Slot>) {
+function FormControl({
+  ...props
+}: React.ComponentProps<typeof SlotPrimitive.Slot>) {
   const { error, formItemId, formDescriptionId, formMessageId } =
     useFormField();
 
